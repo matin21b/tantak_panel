2888d9877ff5080133f55e6dd8e3a9251035aaa7
unknown <mojtabaiie.m76@gmail.com>
2024-08-31 13:15:15 +0330

    شروع طراحی کارتابل

--- pages/panel/index.vue
+++ pages/panel/index.vue
@@ -2,9 +2,143 @@
   <v-container fluid>
     <v-row dense>
       <v-col cols="12" md="12">
-        <v-card class="center-div" >
-          <v-card-title> کارتابل پنل ادمین </v-card-title>
+        <v-card max-width="380" class="elevation-0">
+          <div>محبوبه مجتبائی</div>
+          <div class="my-1">به تن تاک خوش اومدی!</div>
+          <div class="d-flex align-center">
+            <v-icon size="18" color="green">circle</v-icon>
+            <span class="mr-1">آنلاین</span>
+            <v-icon size="20">edit</v-icon>
+          </div>
+          <v-divider color="#fd5800" class="mt-3"></v-divider>
         </v-card>
+      </v-col>
+      <v-col cols="3">
+        <v-card
+          width="110"
+          height="170"
+          class="d-flex flex-column justify-center align-center pa-3 my-2 green-border border-radius-10 elevation-0"
+        >
+          <v-icon>fact_check</v-icon>
+          <div class="my-3 text-center">کارهای امروز من</div>
+          <span>0</span>
+        </v-card>
+        <v-card
+          width="110"
+          height="170"
+          class="d-flex flex-column justify-center align-center pa-3 my-2 green-border border-radius-10 elevation-0"
+        >
+          <v-icon>fact_check</v-icon>
+          <div class="my-3 text-center">کارهای دارای تاخیر</div>
+          <span>0</span>
+        </v-card>
+        <v-card
+          width="110"
+          height="170"
+          class="d-flex flex-column justify-center align-center pa-3 my-2 green-border border-radius-10 elevation-0"
+        >
+          <v-icon>fact_check</v-icon>
+          <div class="my-3 text-center">کارهای قابل پیگیری</div>
+          <span>0</span>
+        </v-card>
+        <v-card
+          width="110"
+          height="170"
+          class="d-flex flex-column justify-center align-center pa-3 my-2 green-border border-radius-10 elevation-0"
+        >
+          <v-icon>fact_check</v-icon>
+          <div class="my-3 text-center">پیام های شخصی من</div>
+          <span>6</span>
+        </v-card>
+      </v-col>
+      <v-col cols="6" style="">
+        <v-sheet class="d-flex align-center gray">
+          <div>همکاران من:</div>
+          <div class="mr-3">
+            <img src="/image/default-user.jpg" width="30" />
+          </div>
+        </v-sheet>
+        <!-- جدول کارتابل -->
+        <table border="1" class="my-6 dashboard-table">
+          <tr>
+            <td>
+              <v-icon>home</v-icon>
+              <span>فروش</span>
+            </td>
+            <td class="d-flex align-center justify-space-between">
+              <div>
+                <v-icon>mode_comment</v-icon>
+                <span>پیام های جدید</span>
+              </div>
+              <div class="primary px-3">2</div>
+            </td>
+          </tr>
+          <!-- ردیف دوم -->
+          <tr>
+            <td>
+              <v-icon>home</v-icon>
+              <span>پروژه ها</span>
+            </td>
+            <td class="d-flex align-center justify-space-between">
+              <div>
+                <v-icon>mode_comment</v-icon>
+                <span>پیام های جدید</span>
+              </div>
+              <div class="primary px-3">2</div>
+            </td>
+          </tr>
+          <!-- ردیف سوم -->
+          <tr>
+            <td>
+              <v-icon>home</v-icon>
+              <span>وظایف شما</span>
+            </td>
+            <td>
+              <div class="d-flex align-center justify-space-between">
+                <div>
+                  <v-icon>mode_comment</v-icon>
+                  <span>پیام های جدید</span>
+                </div>
+                <div class="primary px-3">2</div>
+              </div>
+              <div class="d-flex align-center justify-space-between">
+                <div>
+                  <v-icon>mode_comment</v-icon>
+                  <span>وظایف دارای تاخیر</span>
+                </div>
+                <div class="primary px-3">2</div>
+              </div>
+              <div class="d-flex align-center justify-space-between">
+                <div>
+                  <v-icon>mode_comment</v-icon>
+                  <span>وظایف دارای زمان</span>
+                </div>
+                <div class="primary px-3">2</div>
+              </div>
+              <div class="d-flex align-center justify-space-between">
+                <div>
+                  <v-icon>mode_comment</v-icon>
+                  <span>وظایف بدون زمان</span>
+                </div>
+                <div class="primary px-3">2</div>
+              </div>
+            </td>
+          </tr>
+          <!-- ردیف آخر -->
+          <tr>
+            <td>
+              <v-icon>home</v-icon>
+              <span>نامه ها</span>
+            </td>
+            <td class="d-flex align-center justify-space-between">
+              <div>
+                <v-icon>mode_comment</v-icon>
+                <span>پیام های جدید</span>
+              </div>
+              <div class="primary px-3">2</div>
+            </td>
+          </tr>
+        </table>
       </v-col>
     </v-row>
   </v-container>
@@ -14,7 +148,7 @@
 export default {
   data: () => ({
     title: "کارتابل",
-    roles: []
+    roles: [],
   }),
   computed: {
     person() {
@@ -23,7 +157,7 @@ export default {
         return person;
       }
       return { error: true, message: "error" };
-    }
+    },
   },
   beforeMount() {
     this.changeRout();
@@ -85,7 +219,7 @@ export default {
         return;
       }
     },
-    setCartable(data) {}
-  }
+    setCartable(data) {},
+  },
 };
 </script>
