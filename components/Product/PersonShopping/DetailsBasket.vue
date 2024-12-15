28dd2ca6ad568dc51b960201c812124a84678af3
matin <matinborna138186@gmail.com>
2024-12-15 14:03:32 +0330

    orders

--- components/Product/PersonShopping/DetailsBasket.vue
+++ components/Product/PersonShopping/DetailsBasket.vue
@@ -1,6 +1,8 @@
 <template>
   <v-dialog v-model="dialog" persistent max-width="800">
     <v-card style="overflow: hidden !important" class="pa-5">
+      <div class="card-style pa-5">
+
       <v-row class="d-flex align-center pa-5 mb-4">
         <h1 class="font_18">جزییات سبد خرید</h1>
         <v-spacer></v-spacer>
@@ -8,10 +10,10 @@
           <v-icon> close </v-icon>
         </v-btn>
       </v-row>
-      <v-row class="d-flex justify-center mb-8">
-        <v-col cols="12" md="3" v-if="step == 2">
+      <v-row class="d-flex justify-center mb-3">
+        <v-col cols="12" md="4" v-if="step == 2">
           <amp-button
-            text="موجودی سبد"
+            text=" مشاهده موجودی سبد"
             height="38"
             block
             icon="local_mall"
@@ -21,10 +23,10 @@
           />
         </v-col>
 
-        <v-col cols="12" md="3" v-if="step == 1">
+        <v-col cols="12" md="4" v-if="step == 1">
           <amp-button
             icon="document_scanner"
-            text="تراکنش ها"
+            text= " مشاهده تراکنش ها"
             height="38"
             block
             color="blue-grey"
@@ -36,7 +38,7 @@
       <v-window v-model="step">
         <v-window-item :value="1">
           <v-card-text
-            style="border-right: 3px solid orange"
+            style="border-right: 2px solid grey"
             v-for="(x, i) in data"
             :key="i"
             class="pa-3 elevation-1 my-4"
@@ -77,12 +79,76 @@
         </v-window-item>
 
         <v-window-item :value="2">
+          <div class="text-center my-2" v-if="Object.keys(transactions).length > 0">
+            <v-card
+          
+              v-for="(x, i) in transactions.payments"
+              :key="x.id"
+              class="pa-3 elevation-1 my-4"
+              outlined
+            >
+              <h1>
+                {{ x.text }}
+                <small v-if="Boolean(x.paid_date)">
+                  ( پرداخت شده در
+                  {{ $toJalali(x.paid_date, "YYYY-MM-DD", "jYYYY/jMM/jDD") }}
+                  )
+                </small>
+              </h1>
+              <br />
+
+              <h1>
+                <v-row class="justify-space-between pa-3">
+                  <small>
+                    شماره تراکنش :‌
+                    {{ x.transaction_number }}
+                  </small>
+                  <small> مبلغ تراکنش :‌ {{ $price(x.price) }} ریال </small>
+                </v-row>
+              </h1>
+            </v-card>
+            <v-row>
+              <v-col
+                cols="12"
+                md="6"
+                v-for="(x, i) in transactions.wallet_transactions"
+                :key="x.id"
+                class="text-center"
+              >
+                <v-card
+                  style="border-top: 3px solid grey"
+                  class="pa-3 elevation-1 my-4"
+                  outlined
+                >
+                  <h1>
+                    {{ x.pay_text }}
+                    <small v-if="Boolean(x.paid_date)">
+                      ( پرداخت شده در
+                      {{
+                        $toJalali(x.paid_date, "YYYY-MM-DD", "jYYYY/jMM/jDD")
+                      }}
+                      )
+                    </small>
+                  </h1>
+                  <br />
+
+                  <h1>
+                    <v-icon class="ml-1" small>
+                      done_outline
+                    </v-icon>
+                      <small v-if="x.kind == 'credit'" class="grey--text">
+                        تراکنش انجام شده از کیف پول اعتباری
+                      </small>
+                      <small v-if="x.kind == 'cash'" class="grey--text">
+                        تراکنش انجام شده از کیف پول نقدی
+                      </small>
+                  </h1>
+                </v-card>
+              </v-col>
+            </v-row>
+          </div>
           <div
-            class="text-center"
-            v-if="Object.keys(transactions).length > 0"
-          ></div>
-          <div
-            class="text-center"
+            class="text-center mt-6"
             v-if="
               Object.keys(transactions).length > 0 &&
               transactions.payments.length == 0 &&
@@ -94,6 +160,8 @@
           </div>
         </v-window-item>
       </v-window>
+    </div>
+
     </v-card>
   </v-dialog>
 </template>
@@ -137,3 +205,8 @@ export default {
   },
 };
 </script>
+<style scoped>
+.card-style{
+  border: 8px double #808080a1 !important;
+}
+</style>
