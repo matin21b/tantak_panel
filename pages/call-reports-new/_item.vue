4f20205e02a5f9e562b8f0035d08277114e295e5
Amir THP <code2time@gmail.com>
2025-04-26 10:41:05 +0330

    Rename

--- pages/call-reports-new/_item.vue
+++ pages/call-reports-new/_item.vue
@@ -38,7 +38,7 @@ export default {
   },
   beforeMount() {
     this.$store.dispatch("setPageTitle", this.title);
-    this.body_survey.cdr_id = this.$route.params.survey-item;
+    this.body_survey.cdr_id = this.$route.params.item;
   },
 };
 </script>
